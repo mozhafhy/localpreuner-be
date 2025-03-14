f02b82d516aecb2afe7d5236df12cd13c8b716fd
kaila asus <mochzhafif1123@student.ub.ac.id>
2025-03-15 01:58:25 +0700

    fix: fix file uploaf dto at users

--- src/users/dto/file-upload.dto.ts
+++ src/users/dto/file-upload.dto.ts
@@ -1,12 +1,5 @@
-import { ApiProperty } from '@nestjs/swagger';
-
 export class FileUploadDto {
-  @ApiProperty({ type: File })
   profileImg?: Express.Multer.File;
-
-  @ApiProperty({ type: File })
   catalog?: Express.Multer.File;
-
-  @ApiProperty({ type: File })
   banner?: Express.Multer.File;
 }
